ac5cdf8eac8cc0c80e6a9abec5e8693820a8170d
OmkarParekh <omkarparekh600@gmail.com>
2020-08-31 16:09:50 +0530

    done

--- routes/business.js
+++ routes/business.js
@@ -16,8 +16,8 @@ router.get('/businessapi', getBusiness);
 router.get('/getall', authUser, getAllBusiness);
 router.get('/', authUser, getBusinessByUser);
 router.get('/:businessId', authUser, getBusinessById);
-router.post('/create', authUser,ImgUploader, createBusiness);
-router.put('/update/:businessId',authUser,ImgUploader, updateBusiness);
+router.post('/create', createBusiness);
+router.put('/update/:businessId',ImgUploader, updateBusiness);
 router.delete('/deleteall', deleteAllBusiness);
 router.delete('/delete/:businessId', authUser, deleteBusinessById);
 
